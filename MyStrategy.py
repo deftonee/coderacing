b1fab0018633536ac87eb1edbf781a4bf21a150e
konstantin <deftonee@ya.ru>
2015-11-21 11:58:44 +0300

    работающий поиск пути, надеюсь)

--- MyStrategy.py
+++ MyStrategy.py
@@ -16,6 +16,8 @@ SMALL_SPEED = 0.5
 MEDIUM_SPEED = 4
 MAX_TICKS_WITHOUT_MOVE = 8
 MAX_REAR_MOVE_TICKS = 80
+NEXT_TILE_OFFSET = 0.5
+
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 UPPER = (0, -1)
@@ -35,12 +37,11 @@ class MyStrategy:
         return unit.get_angle_to(x, y) * 32.0 / pi
 
     def steps_to_point(self, ux, uy, px, py):
-        print(ux, uy)
         if ux == px and uy == py:
-            return [(px, py)]
+            return []
         neighbours = [(0, 0), (0, 0), (0, 0), (0, 0)]
-        lx = ux - px
-        ly = uy - py
+        lx = px - ux
+        ly = py - uy
         if abs(lx) >= abs(ly):
             neighbours[0], neighbours[3] = (LEFT, RIGHT) if lx < 0 else (RIGHT, LEFT)
             neighbours[1], neighbours[2] = (UPPER, LOWER) if ly < 0 else (LOWER, UPPER)
@@ -51,50 +52,57 @@ class MyStrategy:
             x = ux + neighbour[0]
             y = uy + neighbour[1]
             if not (0 <= x < self.grid_width and 0 <= y < self.grid_height) or \
-                    self.grid[x][y] not in DEAD_TILES:
+                    self.grid[x][y] in DEAD_TILES:
                 continue
             path = self.steps_to_point(x, y, px, py)
-            if path:
+            if isinstance(path, list):
                 return [(x, y)] + path
         return None
 
+    @staticmethod
+    def move_forward_and_return(move):
+        move.engine_power = 1.0
+        return
+
     def move(self, me: Car, world: World, game: Game, move: Move):
+
         self.grid = world.tiles_x_y
         self.grid_width = len(world.tiles_x_y[0])
         self.grid_height = len(world.tiles_x_y)
 
-        next_tile_type = world.tiles_x_y[me.next_waypoint_x][me.next_waypoint_y]
         curr_tile_x = int(me.x // game.track_tile_size + (
             -1 if me.x % game.track_tile_size == 0 else 0))
         curr_tile_y = int(me.y // game.track_tile_size + (
             -1 if me.y % game.track_tile_size == 0 else 0))
 
+        steps = self.steps_to_point(curr_tile_x, curr_tile_y, me.next_waypoint_x, me.next_waypoint_y)
+        if not steps:
+            return self.move_forward_and_return(move)
+        else:
+            next_tile_x, next_tile_y = steps.pop(0)
+        next_tile_type = world.tiles_x_y[next_tile_x][next_tile_y]
 
-        print('----------', world.tick)
-        if world.tick == 0:
-            for r in world.tiles_x_y:
-                print(r)
-        print(me.next_waypoint_x, me.next_waypoint_y, next_tile_type)
+        # print('----------', world.tick)
+        # if world.tick == 0:
+        #     for j in range(self.grid_width):
+        #         print(', '.join([str(self.grid[i][j]) for i in range(self.grid_height)]))
+        # print(next_tile_x, next_tile_y, next_tile_type)
+        # print(me.next_waypoint_x, me.next_waypoint_y, next_tile_type)
+        # print(self.grid_width, self.grid_height)
         # print(curr_tile_x, curr_tile_y)
-
-        print(self.steps_to_point(curr_tile_x, curr_tile_y, me.next_waypoint_x, me.next_waypoint_y))
-
         # print(self.ticks_without_move, self.rear_move_ticks_remain)
-
-
         # print(me.speed_x, me.speed_y)
         # print(me.x, me.y)
 
         # до начала движения
         if world.tick < game.initial_freeze_duration_ticks:
-            move.engine_power = 1.0
-            return
+            return self.move_forward_and_return(move)
 
         # задний ход
         if self.rear_move_ticks_remain > 0:
             self.rear_move_ticks_remain -= 1
-            next_x = (me.next_waypoint_x + 0.5) * game.track_tile_size
-            next_y = (me.next_waypoint_y + 0.5) * game.track_tile_size
+            next_x = (next_tile_x + NEXT_TILE_OFFSET) * game.track_tile_size
+            next_y = (next_tile_y + NEXT_TILE_OFFSET) * game.track_tile_size
             move.wheel_turn = -1 if self.get_wheel_turn_to_waypoint(me, next_x, next_y) > 0 else 1
             move.engine_power = -1.0
             return
@@ -116,7 +124,7 @@ class MyStrategy:
         for car in world.cars:
             if car.id != me.id and me.get_distance_to_unit(car) <= me.width:
                 cars_is_close = True
-
+        print(cars_is_close)
         if cars_is_close and any((
             all((
                 next_tile_type == TileType.VERTICAL,
@@ -129,11 +137,10 @@ class MyStrategy:
                 abs(me.speed_x) > MEDIUM_SPEED,
             ))
         )):
-            move.engine_power = 1.0
-            return
+            return self.move_forward_and_return(move)
 
-        next_x = (me.next_waypoint_x + 0.5) * game.track_tile_size
-        next_y = (me.next_waypoint_y + 0.5) * game.track_tile_size
+        next_x = (next_tile_x + NEXT_TILE_OFFSET) * game.track_tile_size
+        next_y = (next_tile_y + NEXT_TILE_OFFSET) * game.track_tile_size
 
         corner_tile_offset = 0.25 * game.track_tile_size
 
